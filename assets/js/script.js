7a449259da5e96160770ee9bb7ca8ef01fb363f5
erickreisti <erickreisti@gmail.com>
2022-11-26 22:55:44 -0300

    finalizando formulario

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+"strict mode";
+
+// Funcionando
+
+// function SendMail() {
+//   var params = {
+//     name: document.getElementById("name").value,
+//     email: document.getElementById("email").value,
+//     date: document.getElementById("date").value,
+//     persons: document.getElementById("persons").value,
+//     message: document.getElementById("message").value,
+//   };
+//   emailjs.send("gmailmessage", "template_b8qw9ks", params).then(function () {
+//     alert("Sucesso!");
+//   });
+// }
