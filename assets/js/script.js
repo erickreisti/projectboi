8e0d058723659d569d2d32cbd5ded843866b90b3
erickreisti <erickreisti@gmail.com>
2022-11-27 16:32:11 -0300

    Finalizado envio de email

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,49 @@
 "strict mode";
 
+requestValid = document.querySelector(".info-message");
+btnRequest = document.querySelector(".btnSubmit");
+
 // Funcionando
 
-// function SendMail() {
-//   var params = {
-//     name: document.getElementById("name").value,
-//     email: document.getElementById("email").value,
-//     date: document.getElementById("date").value,
-//     persons: document.getElementById("persons").value,
-//     message: document.getElementById("message").value,
-//   };
-//   emailjs.send("gmailmessage", "template_b8qw9ks", params).then(function () {
-//     alert("Sucesso!");
-//   });
-// }
+function SendMail() {
+  const params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    date: document.getElementById("date").value,
+    persons: document.getElementById("persons").value,
+    message: document.getElementById("message").value,
+  };
+
+  if (Object.values(params).some((e) => !e)) {
+    console.log("error");
+  } else {
+    emailjs.send("gmailmessage", "template_b8qw9ks", params).then(function () {
+      requestValid.classList.remove("hidden");
+      console.log("SUCCESS!");
+    });
+  }
+}
+
+btnRequest.addEventListener("click", SendMail);
+
+// VAIDAÇÃO DE FORMULARIO
+(() => {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
